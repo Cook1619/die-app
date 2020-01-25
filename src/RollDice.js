bdb3431191aee4bbac121a719a02cdcd88807bcf
Cook1619 <cook6348@gmail.com>
2020-01-24 22:20:33 -0600

    Components connected

--- src/RollDice.js
+++ src/RollDice.js
@@ -4,7 +4,7 @@ export default class RollDice extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Dice Roll</h1>
             </div>
         )
     }
