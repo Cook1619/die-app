cd10c91ec76a0ba98492b7878a337ec4b3ffdea8
Cook1619 <cook6348@gmail.com>
2020-01-25 07:58:57 -0600

    Completed dice roll

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
 import './App.css';
 import RollDice from './RollDice'
+import Die from './Die'
 
-function App() {
-  return (
-    <div className="App">
-      <RollDice />
-    </div>
-  );
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  constructor(props){
+    super(props)
+      this.state = {
+        dieNumber: 1
+      }
+  }
+  render() {
+    return (
+      <div className="App">
+        <RollDice />
+      </div>
+    )
+  }
 }
 
-export default App;
--- src/Die.js
+++ src/Die.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
+import './Die.css'
 
-export default function Die() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default class Die extends Component {
+    render() {
+        return (
+            <div>
+                <i className={`Die fas fa-dice-${this.props.face} ${this.props.rolling? 'shake' : ''}`}></i>
+            </div>
+        )
+    }
 }
+
 
